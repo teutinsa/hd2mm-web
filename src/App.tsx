85d789b86859b801a97fed9f5cbe0802044064e0
Florian Schumacher <FloCo12@gmx.net>
2025-03-11 10:38:28 +0100

    Started over on a clean slate.

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,7 @@
-import { useState } from "react";
-import DemoFileUI from "./DemoFileUI";
-import { useTranslation } from "react-i18next";
-import NavBar from "./NavBar";
-import { Button } from "./components/ui/button";
-import { Card } from "./components/ui/card";
-import { ThemeProvider } from "./components/ThemeProvider";
-
 export default function App() {
-  const { t } = useTranslation();
-  
-  //----- State -----
-  const [ acknowledged, setAcknowledged ] = useState(false);
-  
-  //----- Events -----
-  function onOkClick() {
-    setAcknowledged(true);
-  }
-
-  //----- View -----
-  const supported = "showOpenFilePicker" in window;
-  if (!supported) {
-    return (
-      <div className="relative w-screen h-screen flex items-center justify-center">
-        <Card className="p-6 max-w-md shadow-lg">
-          <span className="text-center text-destructive">{t("app.unsupportedError")}</span>
-        </Card>
-      </div>
-    );
-  }
-  
   return (
-    <ThemeProvider defaultTheme="dark">
-      <title>{t("app.title")}</title>
-      {acknowledged ? (
-        <DemoFileUI />
-      ) : (
-        <>
-          <NavBar />
-          <div className="fixed w-full h-full flex items-center justify-center">
-            <Card className="p-6 max-w-md space-y-4">
-              <h3 className="text-center text-xl font-semibold">{t("app.disclaimer.title")}</h3>
-              <p className="text-muted-foreground">{t("app.disclaimer.content")}</p>
-              <Button onClick={onOkClick}>{t("app.disclaimer.button")}</Button>
-            </Card>
-          </div>
-        </>
-      )}
-    </ThemeProvider>
-  );
+    <>
+      
+    </>
+  )
 }
--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,6 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import en from './locales/en.json'
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from "./i18n/en.json"
 
 const resources = {
 	en: { translation: en }
